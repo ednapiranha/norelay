dbb66692057ca9c54b27cc9bcb3bbffee1a4ef0f
Jennifer Fong <jen@ednapiranha.com>
2014-06-08 12:10:24 -0700

    updated express version

--- app.js
+++ app.js
@@ -1,13 +1,26 @@
 var express = require('express');
-var configurations = module.exports;
 var app = express();
-var server = require('http').createServer(app);
+var server = require('http').Server(app);
 var nconf = require('nconf');
-var settings = require('./settings')(app, configurations, express);
+var io = require('socket.io')(server);
+var env = process.env.NODE_ENV || 'development';
+var bodyParser = require('body-parser');
+
+io.on('connection', function (socket) {
+  console.log('connected');
+});
 
 nconf.argv().env().file({ file: 'local.json' });
 
+// Configuration
+app.set('views', __dirname + '/views');
+app.set('view engine', 'jade');
+app.set('view options', { layout: false });
+app.use(bodyParser());
+app.use(express.static(__dirname + '/public'));
+app.locals.pretty = true;
+
 // routes
-require('./routes')(app, nconf);
+require('./routes')(app, io, nconf);
 
 server.listen(process.env.PORT || nconf.get('port'));
